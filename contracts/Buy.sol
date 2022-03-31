0b177ca1d714033c7d3043a7f36fc2789290f6da
韩美丽的电脑 <yuxiangxinxikeji@yuxiangxinxikejideMacBook-Pro.local>
2022-03-31 17:48:02 +0800

    ll

---
+++ contracts/Buy.sol
@@ -0,0 +1,32 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
+contract Buy is Ownable {
+    using SafeERC20 for IERC20;
+    address immutable FUSDT;
+    address immutable FOODNFT;
+
+    mapping(uint256 => uint256) public prices;
+
+    constructor(address fusdt ,address foodnft){
+        FUSDT =  fusdt;
+        FOODNFT  = foodnft;
+    }
+
+    function set_price(uint id , uint price) public onlyOwner{
+        prices[id] = price;
+    }
+
+    function buy(uint id) public{
+        IERC20(FUSDT).safeTransferFrom(msg.sender, address(this), prices[id]);
+        
+        IERC721(FOODNFT).safeTransferFrom(address(this), msg.sender, id);
+    }
+
+
+}
---
+++ contracts/FOODNFT.sol
@@ -0,0 +1,28 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/presets/ERC721PresetMinterPauserAutoId.sol";
+
+contract FOODNFT is ERC721PresetMinterPauserAutoId {
+    mapping(uint256 => uint256) powers;
+
+    constructor()
+        ERC721PresetMinterPauserAutoId("FOODNFT", "NFT", "www.abc.com")
+    {}
+
+    function set_power(uint256 id, uint256 power) public {
+        require(
+            hasRole(DEFAULT_ADMIN_ROLE, msg.sender),
+            "Insufficient permissions"
+        );
+        require(powers[id] == 0, "Cannot change the original power");
+        require(power > 0, "TODO:...");
+        powers[id] = power;
+    }
+
+    function mint_batch(uint256 num, address to) public{
+        for (uint256 i = 0; i < num; i++) {
+            mint(to);
+        }
+    }
+}
