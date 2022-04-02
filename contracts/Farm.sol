0703b9215446c41bc964884d0a158b594684416f
韩美丽的电脑 <yuxiangxinxikeji@yuxiangxinxikejideMacBook-Pro.local>
2022-04-02 14:49:34 +0800

    alsdjfasoidjfoiasdjfiasdjfoajhsdofasodfhasodhfaoshdfoaskjwehiuhasfdifasd

--- contracts/Farm.sol
+++ contracts/Farm.sol
@@ -16,6 +16,7 @@ contract Farm is StakingRewards, Ownable, ERC721Holder {
     mapping(address => uint256) public __balances;
 
     IFarmPower farmpower;
+
     constructor(
         address st,
         address rt,
@@ -23,19 +24,11 @@ contract Farm is StakingRewards, Ownable, ERC721Holder {
     ) StakingRewards(st, rt) {
         farmpower = IFarmPower(fp);
     }
+
     function stake(uint256 id, address pool) public updateReward(pool) {
         uint256 power = IFOODNFT(address(stakingToken)).powers(id);
         require(power > 0, "Power should be greater than 0");
         __balances[pool] += power;
-        // 如果说power=5e18；第一次加入该矿池
-        // farm.calculate = 1.5e18*5 = delte
-        //__balances= 5e
-        //__balances=7.5e
-
-        //别人二次加入该矿池，加1e
-        //__balances=6e
-
-        //_balances =9.6e
         uint256 delta = farmpower.calculate(__balances[pool]) - _balances[pool];
         _totalSupply += delta;
         _balances[pool] += delta;
