658de891ec6df919758a01ed673f1a6efca848ba
jinmingzhu <jinmingzhu@t.com>
2022-06-11 23:00:06 +0800

    using openzeppelin VestingWallet

--- test/TimeLock.js
+++ test/TimeLock.js
@@ -2,53 +2,47 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("TimeLock", function () {
-  it("Should release token base user vest after locktime", async function () {
+  it("Should release token after locktime", async function () {
     const CAT = await ethers.getContractFactory("CAT");
     const cat = await CAT.deploy();
     await cat.deployed();
 
     const signers = await ethers.getSigners();
     const TimeLock = await ethers.getContractFactory("TimeLock");
-    const timeLock = await TimeLock.deploy(cat.address, signers[0].address);
+    const dt = Date.now();
+    const ts = Math.floor(dt / 1000);
+
+    const timeLock = await TimeLock.deploy(signers[1].address, ts + 60, 100);
     await timeLock.deployed();
 
-    await cat.connect(signers[0]).mint(signers[0].address, 1000);
-    expect(await cat.balanceOf(signers[0].address)).to.equal(1000);
+    await cat.connect(signers[0]).mint(timeLock.address, 100);
+    expect(await cat.balanceOf(timeLock.address)).to.equal(100);
   
-    cat.connect(signers[0]).approve(timeLock.address, 1000);
-  
-    timeLock.connect(signers[0]).vest(signers[1].address, 730);
+    await cat.connect(signers[0]).approve(timeLock.address, 100);
+    await hre.network.provider.send("evm_setAutomine", [false]);
 
+    await hre.network.provider.send("evm_setNextBlockTimestamp", [ts + 30]);
+    await hre.network.provider.send("evm_mine");
+    await timeLock.connect(signers[1])["release(address)"](cat.address);
+    released = await timeLock.connect(signers[1])["released(address)"](cat.address);
+    expect(released).to.equal(0);
+    expect(await cat.balanceOf(signers[1].address)).to.equal(0);
+    expect(await cat.balanceOf(timeLock.address)).to.equal(100);
 
-    try {
-      await timeLock.connect(signers[0]).setCurrentTime(365 - 1);
-      await timeLock.connect(signers[1]).release();
-    }
-    catch {
-      console.log("not to release time, still locked");
-      expect(await cat.balanceOf(signers[1].address)).to.equal(0);
-      expect(await cat.balanceOf(signers[0].address)).to.equal(1000);
-    }
+    await hre.network.provider.send("evm_setNextBlockTimestamp", [ts + 60 + 80]);
+    await hre.network.provider.send("evm_mine");
+    await timeLock.connect(signers[1])["release(address)"](cat.address);
+    released = await timeLock.connect(signers[1])["released(address)"](cat.address);
+    expect(released).to.equal(80);
+    expect(await cat.balanceOf(signers[1].address)).to.equal(80);
+    expect(await cat.balanceOf(timeLock.address)).to.equal(20);
 
-    await timeLock.connect(signers[0]).setCurrentTime(365 + 1);
-    await timeLock.connect(signers[1]).release();
-    expect(await cat.balanceOf(signers[1].address)).to.equal(1);
-    expect(await cat.balanceOf(signers[0].address)).to.equal(999);
-
-
-    await timeLock.connect(signers[0]).setCurrentTime(365 * 3);
-    await timeLock.connect(signers[1]).release();
-    expect(await cat.balanceOf(signers[1].address)).to.equal(730);
-    expect(await cat.balanceOf(signers[0].address)).to.equal(270);
-
-    try {
-      await timeLock.connect(signers[0]).setCurrentTime(365 * 3 + 1);
-      await timeLock.connect(signers[1]).release();   
-    }
-    catch {
-      console.log("all your token has released");
-      expect(await cat.balanceOf(signers[1].address)).to.equal(730);
-      expect(await cat.balanceOf(signers[0].address)).to.equal(270);
-    }
+    await hre.network.provider.send("evm_setNextBlockTimestamp", [ts + 60 + 100]);
+    await hre.network.provider.send("evm_mine");
+    await timeLock.connect(signers[1])["release(address)"](cat.address);
+    released = await timeLock.connect(signers[1])["released(address)"](cat.address);
+    expect(released).to.equal(100);
+    expect(await cat.balanceOf(signers[1].address)).to.equal(100);
+    expect(await cat.balanceOf(timeLock.address)).to.equal(0);
   });
 });
